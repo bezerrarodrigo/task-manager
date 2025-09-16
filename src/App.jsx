f69e5ca1955f601bab7922c2b4d2099171c2346e
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-09-16 15:14:36 -0300

    feat: add Sidebar component and integrate Poppins font family

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Sidebar } from './components/Sidebar';
+
 export default function App() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello world!</h1>
+    <div>
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+export function Sidebar() {
+  return (
+    <div className="h-screen w-64 bg-white p-4">
+      <h1>Task Manager</h1>
+    </div>
+  );
+}

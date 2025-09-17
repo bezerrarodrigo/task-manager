4793abeb6242f26eb0dede99ab8f1064a7801fca
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-09-17 09:32:06 -0300

    feat: add prettier-plugin-tailwindcss and update Sidebar component styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,10 @@
 export function Sidebar() {
   return (
     <div className="h-screen w-64 bg-white p-4">
-      <h1>Task Manager</h1>
+      <h1 className="text-xl font-semibold text-teal-500">Task Manager</h1>
+      <p>
+        Um simples <span className="text-teal-500">organizador de tarefas</span>
+      </p>
     </div>
   );
 }

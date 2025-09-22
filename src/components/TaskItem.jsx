43b175055f0a0d8d02c59ac130831eca4377cd83
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-09-22 16:01:44 -0300

    feat: enhance TaskItem component with status icons and improved layout

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,19 +1,41 @@
+import { IconCheck, IconExternalLink, IconProgress } from '@tabler/icons-react';
+
 export function TaskItem({ task }) {
   function getStatusStyles() {
     if (task.status === 'done') {
-      return 'bg-[#00ADB51A] text-[#00ADB5]';
+      return 'bg-[#00ADB5]/10 text-[#00ADB5]';
     } else if (task.status === 'in_progress') {
-      return 'bg-[#FFAA041A] text-[#FFAA04]';
+      return 'bg-[#FFAA04]/10 text-[#FFAA04]';
     } else {
-      return 'bg-[#35383E0D]';
+      return 'bg-[#35383E]/10';
     }
   }
 
   return (
     <div
-      className={`flex items-center gap-3 rounded-[10px] px-4 py-3 text-sm ${getStatusStyles()}`}
+      className={`flex items-center justify-between gap-2 rounded-[10px] px-4 py-3 text-sm ${getStatusStyles()}`}
     >
-      {task.title}
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-6 w-6 cursor-pointer items-center justify-center rounded-lg ${getStatusStyles()}`}
+        >
+          <input
+            checked={task.status === 'done'}
+            type="checkbox"
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === 'done' && (
+            <IconCheck size={16} className="text-white" />
+          )}
+          {task.status === 'in_progress' && (
+            <IconProgress size={16} className="text-amber-500" />
+          )}
+        </label>
+        {task.title}
+      </div>
+      <a className="transition hover:opacity-75" href="#">
+        <IconExternalLink className="text-gray-400" size={18} />
+      </a>
     </div>
   );
 }

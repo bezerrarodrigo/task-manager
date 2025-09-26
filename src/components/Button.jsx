e924061bacdec6107d0e403cb886b39327cd3ef0
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2025-09-26 16:31:24 -0300

    feat: enhance Button component with size and className props for better customization

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,10 @@
-export function Button({ children, variant = 'primary', ...props }) {
+export function Button({
+  children,
+  variant = 'primary',
+  size = 'sm',
+  className,
+  ...props
+}) {
   function getVariantStyles() {
     if (variant === 'ghost') {
       return 'bg-transparent text-[#818181]';
@@ -6,12 +12,24 @@ export function Button({ children, variant = 'primary', ...props }) {
     if (variant === 'primary') {
       return 'bg-[#00ADB5] text-white';
     }
+    if (variant === 'secondary') {
+      return 'bg-[#EEEEEE] text-[#35383E]';
+    }
+  }
+
+  function getSizeStyles() {
+    if (size === 'sm') {
+      return 'py-1 text-xs rounded';
+    }
+    if (size === 'lg') {
+      return 'py-2 text-sm rounded-lg';
+    }
   }
 
   return (
     <button
       {...props}
-      className={`flex items-center gap-1 rounded-md px-3 py-1 text-xs font-semibold transition hover:cursor-pointer hover:opacity-80 ${getVariantStyles()}`}
+      className={`flex items-center justify-center gap-1 px-3 ${getSizeStyles()} font-semibold transition hover:cursor-pointer hover:opacity-80 ${getVariantStyles()} ${className}`}
     >
       {children}
     </button>
